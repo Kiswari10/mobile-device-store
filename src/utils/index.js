6781a1551d77381ed8c34435e56dd7536ee823f2
kiswari10 <kiswari10@gmail.com>
2023-01-03 18:21:17 -0500

    fix and update styles

--- src/utils/index.js
+++ src/utils/index.js
@@ -10,7 +10,7 @@ export const setExpireTime = () => {
 export const availableTime = () => {
   const expireTime = JSON.parse(localStorage.getItem('expire_time'));
   const time = Math.round(new Date().getTime() / 1000);
-  return expireTime && time - expireTime <= 36000;
+  return expireTime && time - expireTime <= 3600;
 };
 
 export const clearLS = (dispatch) => {
